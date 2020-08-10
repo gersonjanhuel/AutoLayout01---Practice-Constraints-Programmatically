6cae8c0c7491f02053ebe6c67c4325b62ac14af6
Gerson Janhuel <gersonjanhuelhuel@gmail.com>
2020-08-10 10:09:58 +0700

    Initial Commit

---
+++ AutoLayout01/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoLayout01
+//
+//  Created by Gerson Janhuel on 10/08/20.
+//  Copyright © 2020 Elcode Studio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
